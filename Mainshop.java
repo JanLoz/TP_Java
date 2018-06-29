3a268b708d1617a85355c2d79aed828d31bf4b25
Formation <janeth.lozano@gmail.com>
2018-06-29 17:23:10 +0200

    shopAmazon

--- Mainshop.java
+++ Mainshop.java
@@ -3,9 +3,15 @@ package com.m2i.poe.shop;
 public class Mainshop {
 
     public static void main (String [] args) {
-        Media m =new Book("1", "Moby Dick",10, "Folio","Fiction" ); //create an object
+        Media m1 =new Book("1", "Moby Dick",10, "Folio","Fiction" ); //create an object
+        System.out.println(m1.toString());
+        System.out.println(m1.getNetPrice());
+        m1.setPublisher(new Publisher(4,"Folio"));
+        System.out.println(m1.getPublisher());
+        m1.getAuthorList().add(new Author(5, "Herman", "Melville"));
 
-        System.out.println(m.getNetPrice());
-
+        Media m2 = new Cd("10", "Orchestra", 15);
+        System.out.println(m2.toString());
+        System.out.println(m2.getNetPrice());
     }
 }
